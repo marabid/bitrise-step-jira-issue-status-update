4fdf2904d0cb7ef78e8c09429f593b2172d70a95
Michał Sikora <me@mehow.io>
2020-02-09 15:58:05 +0100

    Add communication with JIRA REST.

---
+++ jiraservice.go
@@ -0,0 +1,163 @@
+package main
+
+import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/bitrise-io/go-utils/log"
+	"github.com/bitrise-io/go-utils/urlutil"
+)
+
+type jiraService struct {
+	client     http.Client
+	baseURL    string
+	authHeader string
+}
+
+func createService(user string, token string, baseURL string) *jiraService {
+	basicAuth := []byte(user + `:` + token)
+	return &jiraService{
+		client: http.Client{
+			Timeout: time.Second * 10,
+		},
+		baseURL:    baseURL,
+		authHeader: "Basic " + base64.StdEncoding.EncodeToString(basicAuth),
+	}
+}
+
+func (service jiraService) getAvailableTransitions(issueKey string) (*Transitions, error) {
+	httpURL, err := urlutil.Join(service.baseURL, "rest/api/3/issue", issueKey, "transitions")
+	if err != nil {
+		return nil, err
+	}
+
+	request, err := http.NewRequest(http.MethodGet, httpURL, nil)
+	if err != nil {
+		return nil, err
+	}
+	request.Header.Set("Authorization", service.authHeader)
+	request.Header.Set("Content-Type", "application/json")
+
+	response, err := service.client.Do(request)
+	if err != nil {
+		return nil, err
+	}
+	defer silentClose(response.Body)
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var transitions Transitions
+	err = json.Unmarshal(body, &transitions)
+	if err != nil {
+		return nil, err
+	}
+
+	return &transitions, nil
+}
+
+func (service jiraService) makeTransition(issueKey string, transition Transition) error {
+	httpURL, err := urlutil.Join(service.baseURL, "rest/api/3/issue", issueKey, "transitions")
+	if err != nil {
+		return err
+	}
+
+	requestBody, err := json.Marshal(TransitionRequestBody{
+		Transition: transition,
+	})
+	if err != nil {
+		return err
+	}
+
+	request, err := http.NewRequest(http.MethodPost, httpURL, bytes.NewBuffer(requestBody))
+	if err != nil {
+		return err
+	}
+	request.Header.Set("Authorization", service.authHeader)
+	request.Header.Set("Content-Type", "application/json")
+
+	response, err := service.client.Do(request)
+	if err != nil {
+		return err
+	}
+	defer silentClose(response.Body)
+
+	return nil
+}
+
+// To ...
+type To struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+func (to To) String() string {
+	return fmt.Sprintf("To(ID=%s, Name=%s)", to.ID, to.Name)
+}
+
+// Transition ...
+type Transition struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+	To   To     `json:"to"`
+}
+
+func (transition Transition) String() string {
+	return fmt.Sprintf("Transition(ID=%s, Name=%s, To=%s)", transition.ID, transition.Name, transition.To)
+}
+
+// Transitions ...
+type Transitions struct {
+	Transitions []Transition `json:"transitions"`
+}
+
+func (transitions Transitions) String() string {
+	var slice []string
+	for _, transition := range transitions.Transitions {
+		slice = append(slice, transition.String())
+	}
+	return fmt.Sprintf("[%s]", strings.Join(slice, ", "))
+}
+
+func (transitions Transitions) findTransition(toID string) (*Transition, error) {
+	var matchingTransitions []Transition
+	for _, transition := range transitions.Transitions {
+		if transition.To.ID == toID {
+			matchingTransitions = append(matchingTransitions, transition)
+		}
+	}
+
+	transitionsCount := len(matchingTransitions)
+	if transitionsCount <= 0 {
+		return nil, fmt.Errorf("No matching transitions found. Available transitions: %s", transitions)
+	} else if transitionsCount > 1 {
+		var slice []string
+		for _, transition := range transitions.Transitions {
+			slice = append(slice, transition.String())
+		}
+		return nil, fmt.Errorf("More than 1 matching transition found. Matching transitions: %s", strings.Join(slice, ", "))
+	} else {
+		return &matchingTransitions[0], nil
+	}
+}
+
+// TransitionRequestBody ...
+type TransitionRequestBody struct {
+	Transition Transition `json:"transition"`
+}
+
+func silentClose(closer io.Closer) {
+	err := closer.Close()
+	if err != nil {
+		log.Warnf("Failed to close, error: %s", err)
+	}
+}
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/bitrise-io/go-utils/log"
 	"github.com/bitrise-tools/go-steputils/stepconf"
@@ -25,6 +26,34 @@ func main() {
 
 	stepconf.Print(cfg)
 	fmt.Println()
+
+	service := createService(cfg.UserName, cfg.APIToken, cfg.BaseURL)
+
+	for _, issueKey := range strings.Split(cfg.IssueKeys, `|`) {
+		log.Infof("Checking issue %s status", issueKey)
+
+		transitions, err := service.getAvailableTransitions(issueKey)
+		if err != nil {
+			log.Warnf("Failed to get status for issue %s, error: %s", issueKey, err)
+			continue
+		}
+
+		transition, err := transitions.findTransition(cfg.ToStatus)
+		if err != nil {
+			log.Warnf("Failed to update status for issue %s, error: %s", issueKey, err)
+			continue
+		}
+
+		err = service.makeTransition(issueKey, *transition)
+		if err != nil {
+			log.Warnf("Failed to update status for issue %s, error: %s", issueKey, err)
+			continue
+		}
+
+		log.Infof("Successfully updated issue %s to status %s", issueKey, cfg.ToStatus)
+
+		fmt.Println()
+	}
 }
 
 func failf(message string, arguments ...interface{}) {
